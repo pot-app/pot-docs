f6119c1ec07adc4ad2c96f86a1217c59c273ece8
Pylogmon <pylogmon@outlook.com>
2023-11-19 12:02:54 +0800

    feat: Add Comment Component

--- vitepress/.vitepress/theme/index.js
+++ vitepress/.vitepress/theme/index.js
@@ -2,6 +2,8 @@
 import { h } from "vue";
 import Theme from "vitepress/theme";
 import AppPreview from "./components/AppPreview.vue";
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
 import "./style.css";
 
 /** @type {import('vitepress').Theme} */
@@ -16,4 +18,21 @@ export default {
   enhanceApp({ app, router, siteData }) {
     // ...
   },
+  setup() {
+    const { frontmatter } = useData();
+    const route = useRoute();
+    giscusTalk({
+      repo: 'pot-app/pot-desktop',
+      repoId: 'R_kgDOJCp3Ww',
+      category: 'General',
+      categoryId: 'DIC_kwDOJCp3W84CUrQN',
+      mapping: 'title',
+      inputPosition: 'top',
+      lang: 'zh-CN',
+    }, {
+      frontmatter, route
+    },
+      true
+    );
+  }
 };
